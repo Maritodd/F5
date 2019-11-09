a8905131d145ef167fbdfb4e5d752e03a858268e
yaroslav <yaroslav9190@gmail.com>
2019-11-09 22:31:36 +0200

    6x part

---
+++ RB-6/137task-b.rb
@@ -0,0 +1,10 @@
+def task137()
+  n = gets.to_i
+  q=0.0
+  for i in 1..n
+    q += 1 + i
+  end
+  puts q.abs
+end
+
+task137()
---
+++ RB-6/148task.rb
@@ -0,0 +1,11 @@
+def task148()
+  r = 0.0
+  for i in 1..100
+    r = (9/5)*i +32
+    p r
+  end
+end
+
+task148()
+
+
