25c0913bd6bfa9f4390b1b5713266fed4402dc92
yaroslav <yaroslav9190@gmail.com>
2019-11-23 11:53:53 +0200

    par14

---
+++ RB-14/531-task.rb
@@ -0,0 +1,8 @@
+def task531(n)
+  for i in 2..n
+    q = i-n
+    p q
+  end
+end
+
+task531(3)
---
+++ RB-14/532-task.rb
@@ -0,0 +1,7 @@
+def task532()
+  x = [1, 4, 3, 5, 2]
+  y = x.sort { |a, b| b <=> a }
+  puts y
+end
+
+task532()
